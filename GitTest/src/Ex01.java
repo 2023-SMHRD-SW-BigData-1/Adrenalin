a102177e9f3c67ce2e8e03507ff8a6c867406afa
smhrd <smhrd@DESKTOP-MFJ1TMB>
2023-05-09 11:41:28 +0900

    대충 보기2번까지

--- GitTest/src/Ex01.java
+++ GitTest/src/Ex01.java
@@ -22,6 +22,15 @@ public class Ex01 {
 				System.out.print("이름을 입력하세요 : ");
 				int name = sc.nextInt();
 		// 2. 보기를 통해 상황을 부여하여 탈출하는 방식
+				System.out.println(name+"은 종례시간이 되었다. 어떻게 할까?");
+				System.out.println("1. 지문을 찍고 집에가서 잔다.    2. 지문은 제끼고 후다닥 나간다.");
+				int num1 = sc.nextInt();
+				if(num1==1) {
+					System.out.println("당신은 모범생입니다. 이 게임을 즐길 자격이 없습니다. 사망.");
+					
+				}else {
+					System.out.println("저녁 9시 게임을 하는중 전화가 온다. 정세연 선생님");
+				}
 				
 		// 3. 각자 한층씩 상황을 상상하여 만든 후 상의
 		
