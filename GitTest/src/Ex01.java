a56851203116901bbe464c268e78de1118d39dce
smhrd <smhrd@DESKTOP-816TFUE>
2023-05-09 11:59:22 +0900

    없앰

--- GitTest/src/Ex01.java
+++ GitTest/src/Ex01.java
@@ -4,18 +4,6 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 
-		System.out.println("팀장 첫번째 커밋");
-		System.out.println("팀원1 두번째 커밋");
-		System.out.println("팀원2 예뿌니 세번째 커밋 ");
-		System.out.println("팀원3 네번째 커밋");
-		
-		
-		//주석도 커밋이 되나용??????????????
-		
-		
-		System.out.println("키킼ㅋ키");
-		System.out.println("왜 나는 주석 문자 안오냐고");
-		
 		// 미니프로젝트 주제: 학원탈출
 		// 1. 스캐너 이용해서 이름 입력 받고 이름 나오는 부분 전부 name변수 처리
 				Scanner sc = new Scanner(System.in);
