f22b4c6d215f37f971569425f72814087e0e5f7c
smhrd <smhrd@DESKTOP-MFJ1TMB>
2023-05-09 12:46:20 +0900

    6층 도착까지 커밋.

--- GitTest/src/Ex01.java
+++ GitTest/src/Ex01.java
@@ -16,6 +16,7 @@ public class Ex01 {
 
 		// 미니프로젝트 주제: 학원탈출
 		// 1. 스캐너 이용해서 이름 입력 받고 이름 나오는 부분 전부 name변수 처리
+		int hp = 100;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("이름을 입력하세요 : ");
 		int name = sc.nextInt();
@@ -39,7 +40,48 @@ public class Ex01 {
 				System.out.println();
 				System.out.println("당신은 1층에 도착했다. 어떻게 올라갈 것인가?");
 				System.out.println("1. 엘레베이터를 타고 간다.   2. 계단으로 올라간다.");
-				
+				int num3 = sc.nextInt();
+				if (num3 == 2) {
+					System.out.println("당신은 객기를 부려 체력이 감소하였습니다. 체력 -20");
+					hp -= 20;
+					System.out.println("현재 체력 : " + hp);
+				} else if (num3 == 1) {
+					System.out.println("아~ 편안하다 엘레베이터 최고당");
+					System.out.println("현재 체력 : " + hp);
+				}
+				System.out.println("================================");
+				System.out.println("7층 도착.");
+				while (true) {
+					System.out.println("지문이나 찍자~ 빨리 집가서 게임해야징~~");
+					System.out.println("1. 지문을 찍는다.   2. 지문을 찍지 않고 돌아다닌다.");
+					int num4 = sc.nextInt();
+					if (num4 == 2) {
+						System.out.println("내가 여기 뭐하러 왔더라.. 흠...");
+						hp -= 10;
+						System.out.println("현재 체력 : " + hp);
+					} else if (num4 == 1) {
+						System.out.println("쾅!!! 엘레베이터에서 굉음이 일어났다.");
+						System.out.println("현재 체력 : " + hp);
+						break;
+					}
+				}
+				while (true) {
+					System.out.println("헐 뭐야 어떻게 하지??");
+					System.out.println("1. 엘레베이터 열림버튼을 계속 누른다.  2. 계단으로 내려간다. ");
+					int num5 = sc.nextInt();
+					if (num5 == 1) {
+						System.out.println("아 안열리네.. 손아프당..");
+						hp -= 10;
+						System.out.println("현재 체력 : " + hp);
+					} else if (num5 == 2) {
+						System.out.println("어두 컴컴하네.. 6층으로 가보자!");
+						System.out.println("현재 체력 : " + hp);
+						break;
+					}
+				}
+				System.out.println("=======================");
+				System.out.println("6층 도착.");
+
 			}
 
 		}
