e4841a84f8e635a7b60a14bcc14ffb43b8f26487
smhrd <smhrd@DESKTOP-816TFUE>
2023-05-10 16:50:20 +0900

    Merge branch 'master' of https://github.com/2023-SMHRD-SW-BigData-1/Adrenalin.git

--- GitTest/src/View/CardGame_main.java
+++ GitTest/src/View/CardGame_main.java
@@ -8,8 +8,9 @@ public class CardGame_main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+
 		System.out.println("=====블랙잭 게임!!!====");
-		
+
 		System.out.println("당신은 블랙잭 게임에 참여하였습니다.");
 		System.out.println("당신과 상대방의 카드에는 랜덤의 숫자로 각각 5장씩 부여됩니다");
 		System.out.println("그 중 가장 높은 번호의 카드 2장을 선택하세요");
@@ -18,38 +19,36 @@ public class CardGame_main {
 		System.out.println("상대방 카드의 합 보다 작을경우 당신이 게임에 참여할 수 있는 기회는 한번씩 사라지게 될것입니다.");
 		System.out.println("상대방 보다 높은 숫자가 나올때마다 당신은 +3점씩 점수를 얻게 됩니다.");
 		System.out.println("10점이상의 점수를 얻을 경우 당신은 최종 승리를 하게 됩니다.");
-		
+		System.out.println();
+
 		System.out.print("게임에 참여하시겠습니까?(y or n) >> ");
+
 		String choice = sc.next();
-		if(choice =="y") { 
-			 
-		}else if(choice=="n") {
-			System.out.println("잘가세요.");
-		}
-		
-		 
-		while(true) {
+
+		if (choice.equals("n")) {
+			System.out.println("다시한번 물어봅니다. 게임 하기 싫습니까?");
+
+			String choice2 = sc.next();
+
+			if (choice2.equals("y")) {
+				System.out.println("잘가세연.");
+			} else if (choice.equals("n")) {
+				System.out.println("탁월한 선택입니다. 게임을 시작하지요");
+			}
+		} else if(choice.equals("y")) {
+
 			System.out.print("[1] 회원가입 [2] 로그인 [3]게임시작 : ");
-			
+
 			int num = sc.nextInt();
- 
-			if(num==1) {
+
+			if (num == 1) {
 				System.out.print("id 입력 : ");
 				String id = sc.next();
 				System.out.print("pw 입력 : ");
 				String pw = sc.next();
-				 
+
 			}
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-
 	}
 
 }
