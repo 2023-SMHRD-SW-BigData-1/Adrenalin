477371f59d38cec64fe027164793af74d06b43ff
smhrd <smhrd@DESKTOP-MFJ1TMB>
2023-05-10 12:46:43 +0900

    걍만들기 커밋

--- GitTest/src/View/CardGame_main.java
+++ GitTest/src/View/CardGame_main.java
@@ -29,24 +29,7 @@ public class CardGame_main {
 		
 		
 		
-		Random rd = new Random();
-
-		int[] arr = new int[5];
-
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = rd.nextInt(10) + 1;
-
-			for (int j = 0; j < i; j++) {
-				if (arr[i] == arr[j]) {
-					i--;
-					break;
-				}
-			}
-		}
-
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println("카드 숫자 : " + arr[i]);
-		}
+		
 
 	}
 
